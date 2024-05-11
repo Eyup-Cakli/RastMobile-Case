8f8fc26c98754392228aa48f25194ec5ea7ef3f1
Eyup Cakli <eyupcakli@hotmail.com>
2024-05-11 19:11:35 +0300

    feat: header component is created

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [
-    MatButtonModule,
-    MatIconModule
-  ],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+    selector: 'app-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.scss',
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        HeaderComponent
+    ]
 })
 export class HomePageComponent {
 
